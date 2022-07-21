f787519fceea7dfcfc2bd063319c0d45f22b89e9
hector <hectornuno1994@gmail.com>
2022-07-21 14:08:35 -0700

    adds header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
       <h1> lets build the amazon store 🚀</h1>
+      <Header />
+      {/* home */}
     </div>
   );
 }
