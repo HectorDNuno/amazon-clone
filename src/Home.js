0397af51c2f247ae4beac36782dc0ce0bb650f68
hector <hectornuno1994@gmail.com>
2022-07-21 15:10:56 -0700

    adds home background image, sets up divs that are going to display products

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,26 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <div className="home__container"></div>
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt="background for home"
+        />
+      </div>
+
+      <div className="home__row">
+        {/* product */}
+        {/* product */}
+      </div>
+
+      <div className="home__row">
+        {/* product */}
+        {/* product */}
+        {/* product */}
+      </div>
+
+      <div className="home__row">{/* product */}</div>
     </div>
   );
 }
