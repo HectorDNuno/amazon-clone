f6301c2c1f2eae07fad84aca6a6640e2609a030b
hector <hectornuno1994@gmail.com>
2022-07-21 14:25:11 -0700

    removes placeholer div

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./Header";
 function App() {
   return (
     <div className="app">
-      <h1> lets build the amazon store 🚀</h1>
       <Header />
       {/* home */}
     </div>
