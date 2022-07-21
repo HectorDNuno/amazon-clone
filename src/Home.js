b0e02600d6c9c077a105dbb0cc4155ac9664b6df
hector <hectornuno1994@gmail.com>
2022-07-21 15:55:55 -0700

    moves divs to be under home__container

--- src/Home.js
+++ src/Home.js
@@ -11,20 +11,19 @@ function Home() {
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt="background for home"
         />
-      </div>
+        <div className="home__row">
+          <Product />
+          <Product />
+        </div>
 
-      <div className="home__row">
-        <Product />
-        <Product />
-      </div>
+        <div className="home__row">
+          {/* product */}
+          {/* product */}
+          {/* product */}
+        </div>
 
-      <div className="home__row">
-        {/* product */}
-        {/* product */}
-        {/* product */}
+        <div className="home__row">{/* product */}</div>
       </div>
-
-      <div className="home__row">{/* product */}</div>
     </div>
   );
 }
