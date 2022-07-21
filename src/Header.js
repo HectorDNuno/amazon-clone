f9c5c2e705832a456ad1eadc9c58e46ce0ba1d74
hector <hectornuno1994@gmail.com>
2022-07-21 14:49:57 -0700

    adds basket icon and basket option to nav bar

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
   return (
@@ -27,6 +28,11 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
+      </div>
+
+      <div className="header__optionBasket">
+        <ShoppingBasketIcon />
+        <span className="header_optionLineTwo header__basketCount">0</span>
       </div>
     </div>
   );
