a57da41fe791fbc352c52e4d839d79bf5c343acd
hector <hectornuno1994@gmail.com>
2022-07-24 15:52:07 -0700

    removes console.log

--- src/Product.js
+++ src/Product.js
@@ -4,7 +4,6 @@ import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
   const [{ cart }, dispatch] = useStateValue();
-  console.log("this is the cart >>>", cart);
 
   const addToCart = () => {
     //sends item to data layer
