fd0672362a1f4de9fde9867f86c3f2f374da295d
hector <hectornuno1994@gmail.com>
2022-07-24 11:54:23 -0700

    imports firebase and creates and exports db and auth variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBNJXEP5s4v72WzTHveSNMXEtW_3PZ-1HU",
   authDomain: "clone-a8546.firebaseapp.com",
@@ -8,3 +9,10 @@ const firebaseConfig = {
   appId: "1:423025121548:web:fac9ae54142203505dd129",
   measurementId: "G-ZMQ41S1MP7",
 };
+
+const firebaseApp = firebase.initalizeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
