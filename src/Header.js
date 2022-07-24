b373313d8cb39080845ec73ac75135b65f2692a8
hector <hectornuno1994@gmail.com>
2022-07-23 18:02:04 -0700

    adds cart to update how many items in cart

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  const [{ cart }] = useStateValue();
+
   return (
     <div className="header">
       <Link to="/">
@@ -36,7 +39,7 @@ function Header() {
       <Link to="/checkout">
         <div className="header__optionBasket">
           <ShoppingBasketIcon />
-          <span className="header_optionLineTwo header__basketCount">0</span>
+          <span className="header_optionLineTwo header__basketCount"> {cart?.length} </span>
         </div>
       </Link>
     </div>
