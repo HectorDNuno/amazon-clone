d61d56b21298cc1f8432cc952ec1ab8d225bb5ee
hector <hectornuno1994@gmail.com>
2022-07-24 10:59:06 -0700

    creates login component and css file

---
+++ src/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+
+      <dib className="login__container"></dib>
+    </div>
+  );
+}
+
+export default Login;
