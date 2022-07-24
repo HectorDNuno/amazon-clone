ac1ff6783cd25f6633c53e179c63237676d72728
hector <hectornuno1994@gmail.com>
2022-07-24 12:23:11 -0700

    changes import statements to fit version 9 of firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBNJXEP5s4v72WzTHveSNMXEtW_3PZ-1HU",
@@ -6,11 +8,11 @@ const firebaseConfig = {
   projectId: "clone-a8546",
   storageBucket: "clone-a8546.appspot.com",
   messagingSenderId: "423025121548",
-  appId: "1:423025121548:web:fac9ae54142203505dd129",
-  measurementId: "G-ZMQ41S1MP7",
+  appId: "1:423025121548:web:c585ed415e5c8d3c5dd129",
+  measurementId: "G-N1HJ820D8J",
 };
 
-const firebaseApp = firebase.initalizeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
