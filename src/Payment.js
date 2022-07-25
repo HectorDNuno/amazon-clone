372765c9fcd57d747c3761120b07e6e96ee5dedc
hector <hectornuno1994@gmail.com>
2022-07-24 17:20:30 -0700

    adds sections of the payments component

--- src/Payment.js
+++ src/Payment.js
@@ -1,7 +1,10 @@
 import React from "react";
+import CheckoutProduct from "./CheckoutProduct";
 import "./Payment.css";
+import { useStateValue } from "./StateProvider";
 
 function Payment() {
+  const [{ cart, user }, dispatch] = useStateValue();
   return (
     <div className="payment">
       <div className="payment__container">
@@ -9,9 +12,36 @@ function Payment() {
           <div className="payment__title">
             <h3>Delivery Address</h3>
           </div>
+          <div className="payment__address">
+            <p> {user?.email} </p>
+            <p>123 React Lane</p>
+            <p>Los Angeles, CA</p>
+          </div>
         </div>
-        <div className="payment__section"></div>
-        <div className="payment__section"></div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {cart?.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details"></div>
+        </div>
       </div>
     </div>
   );
