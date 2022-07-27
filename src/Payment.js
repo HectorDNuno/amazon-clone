3e339ad4b2e1312606186ab13fc492b29c6fca24
hector <hectornuno1994@gmail.com>
2022-07-27 15:35:31 -0700

    imports functions from stripe and begins the payment processing code

--- src/Payment.js
+++ src/Payment.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CheckoutProduct from "./CheckoutProduct";
 import "./Payment.css";
@@ -6,6 +7,21 @@ import { useStateValue } from "./StateProvider";
 
 function Payment() {
   const [{ cart, user }, dispatch] = useStateValue();
+  const [erroe, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = (e) => {
+    // stripe stuff
+  };
+
+  const handleChange = (event) => {
+    //listen for changes and display errors as customer types
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
 
   return (
     <div className="payment">
@@ -46,7 +62,11 @@ function Payment() {
           <div className="payment__title">
             <h3>Payment Method</h3>
           </div>
-          <div className="payment__details">{/* stripe goes here */}</div>
+          <div className="payment__details">
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+            </form>
+          </div>
         </div>
       </div>
     </div>
