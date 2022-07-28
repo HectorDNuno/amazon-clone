a5d2ea70004268e40131e8e46420934b54d17d43
hector <hectornuno1994@gmail.com>
2022-07-27 17:27:27 -0700

    creates backend with express

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51LQIV7JmV3eGDMI7MRIqpxxW4rRJbLrDLZRJjwYsVlUxAP4WTNv3le1PgPDWW6sczcRq7PFh0c3rnJHtFmueOPt8003JyfmnAO"
+);
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+  console.log("payment request recieved", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "USD",
+  });
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+exports.api = functions.https.onRequest(app);
+// http://localhost:5001/clone-a8546/us-central1/api
