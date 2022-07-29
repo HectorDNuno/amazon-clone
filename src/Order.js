db6962b79c836b01e0735dfbec8b5b9546f946c5
hector <hectornuno1994@gmail.com>
2022-07-29 14:08:16 -0700

    imports currency format to show total price of order and adds prop to checkout product that hides button when checkout product is in order

--- src/Order.js
+++ src/Order.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Order.css";
 import moment from "moment";
 import CheckoutProduct from "./CheckoutProduct";
+import CurrencyFormat from "react-currency-format";
 
 function Order({ order }) {
   return (
@@ -13,8 +14,24 @@ function Order({ order }) {
         <small> {order.id} </small>
       </p>
       {order.data.cart?.map((item) => (
-        <CheckoutProduct id={item.id} title={item.title} image={item.image} price={item.price} rating={item.rating} />
+        <CheckoutProduct
+          id={item.id}
+          title={item.title}
+          image={item.image}
+          price={item.price}
+          rating={item.rating}
+          hideButton
+        />
       ))}
+
+      <CurrencyFormat
+        renderText={(value) => <h3>Order Total: {value}</h3>}
+        decimalScale={2}
+        value={order.data.amount / 100}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
     </div>
   );
 }
