34ccd7d3290fd2dcf2ee49c9dcacd2279f9ea9b6
hector <hectornuno1994@gmail.com>
2022-08-18 13:39:10 -0700

    begins coding compare component

--- src/Compare.js
+++ src/Compare.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { products } from "./ProductData";
+import "./Compare.css";
 
 function Compare() {
-  return <div className="compare">Compare</div>;
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  return (
+    <div className="compare">
+      <h1>Compare Products</h1>
+
+      <ul onClick={handleClick} className={click ? "dropdown-menu-clicked" : "dropdown-menu"}>
+        {products.map((product) => (
+          <li className="dropdown-link">{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Compare;
