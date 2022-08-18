7449100b3a2a799b93d256f9285b22802bcbd39e
hector <hectornuno1994@gmail.com>
2022-08-18 15:31:25 -0700

    creates component to compare selected product

--- src/ComparedProduct.js
+++ src/ComparedProduct.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { products } from "./ProductData";
+import "./ComparedProduct.css";
 
-function ComparedProduct({ productId }) {
-  return <div> {productId} </div>;
+function ComparedProduct({ comparedProduct }) {
+  return (
+    <div className="comparedProduct">
+      {products.map((product) => {
+        return (
+          <>
+            {product.id === comparedProduct && (
+              <div className="comparedProduct__details">
+                <p>{product.title}</p>
+                <p>{product.price}</p>
+                <p>{product.rating}</p>
+                <img src={product.image} alt="" />
+              </div>
+            )}
+          </>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ComparedProduct;
