4b1f851e9fab0b965ebebbc1b13915d32949eb4a
hector <hectornuno1994@gmail.com>
2022-08-18 13:39:52 -0700

    adds seperate file for product data

---
+++ src/ProductData.js
@@ -0,0 +1,33 @@
+export const products = [
+  {
+    id: "12321341",
+    title: "Nintendo Switch with Neon Blue and Neon Red Joy-Con",
+    price: 299.0,
+    rating: 5,
+    image: "https://m.media-amazon.com/images/I/61-PblYntsL._AC_SL1500_.jpg",
+  },
+  {
+    id: "49538094",
+    title:
+      "GE Profile Opal | Countertop Nugget Ice Maker | Portable Ice Machine Makes up to 24 lbs. of Ice Per Day | Stainless Steel Finish",
+    price: 479.0,
+    rating: 4,
+    image: "https://m.media-amazon.com/images/I/810et96FfhL._AC_SL1500_.jpg",
+  },
+  {
+    id: "4903850",
+    title:
+      "Brother P-touch, PTD210, Easy-to-Use Label Maker, One-Touch Keys, Multiple Font Styles, 27 User-Friendly Templates, White",
+    price: 59.98,
+    rating: 3,
+    image: "https://m.media-amazon.com/images/I/81qdOn+31VL._AC_SL1500_.jpg",
+  },
+  {
+    id: "23445930",
+    title:
+      "WD_BLACK 1TB SN850 NVMe Internal Gaming SSD Solid State Drive with Heatsink - Works with Playstation 5, Gen4 PCIe, M.2 2280, Up to 7,000 MB/s - WDS100T1XHE",
+    price: 130.0,
+    rating: 5,
+    image: "https://m.media-amazon.com/images/I/719mkEAj1IL._AC_SL1500_.jpg",
+  },
+];
