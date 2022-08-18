b35750e30cb49bde0b42980e7dc1fa64001732bf
hector <hectornuno1994@gmail.com>
2022-08-18 14:25:33 -0700

    renames compare component

---
+++ src/CompareProducts.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { products } from "./ProductData";
+import "./Compare.css";
+import ComparedProduct from "./ComparedProduct";
+
+function CompareProducts() {
+  const [selectedProduct, setSelectedProduct] = useState("");
+
+  const handleSelect = (e) => {
+    setSelectedProduct(e.target.value);
+  };
+
+  return (
+    <div className="compare">
+      <h1>Compare Products</h1>
+
+      <select name="selectList" id="selectList" onChange={handleSelect}>
+        <option value="Select a product" disabled selected>
+          Select a product
+        </option>
+        {products.map((product) => (
+          <option value={product.id} key={product.id}>
+            {product.title}
+          </option>
+        ))}
+      </select>
+
+      <ComparedProduct productId={selectedProduct} />
+    </div>
+  );
+}
+
+export default CompareProducts;
