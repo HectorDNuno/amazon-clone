7df92ff400a95f23c97e9d24b6f9642e3d2f84e8
hector <hectornuno1994@gmail.com>
2022-08-25 12:54:02 -0700

    rearranges the way product info appears

--- src/CompareProducts.js
+++ src/CompareProducts.js
@@ -10,23 +10,49 @@ function CompareProducts() {
     setSelectedProduct(e.target.value);
   };
 
+  const [selectedProduct2, setSelectedProduct2] = useState();
+
+  const handleSelect2 = (e) => {
+    setSelectedProduct2(e.target.value);
+  };
+
   return (
     <div className="compare">
       <h1>Compare Products</h1>
 
-      <select name="selectList" id="selectList" onChange={handleSelect}>
-        <option value="Select a product" disabled selected>
-          Select a product
-        </option>
+      <div className="selections">
+        <div className="first__selection">
+          <select name="selectList" id="selectList" onChange={handleSelect}>
+            <option value="Select a product" disabled selected>
+              Select a product
+            </option>
 
-        {products.map((product) => (
-          <option value={product.id} key={product.id}>
-            {product.title}
-          </option>
-        ))}
-      </select>
+            {products.map((product) => (
+              <option value={product.id} key={product.id}>
+                {product.title}
+              </option>
+            ))}
+          </select>
 
-      <ComparedProduct comparedProduct={selectedProduct} />
+          <ComparedProduct comparedProduct={selectedProduct} />
+        </div>
+
+        <div className="second__selection">
+          <select name="selectList" id="selectList" onChange={handleSelect2}>
+            <option value="Select a product" disabled selected>
+              Select a product
+            </option>
+
+            {products.map((product) => (
+              <option value={product.id} key={product.id}>
+                {product.title}
+              </option>
+            ))}
+          </select>
+
+          <ComparedProduct comparedProduct={selectedProduct2} />
+        </div>
+      </div>
     </div>
   );
 }
