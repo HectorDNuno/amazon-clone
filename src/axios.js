250a35fdc06b05e3a7496470f6b4a33aed841a32
hector <hectornuno1994@gmail.com>
2022-09-29 13:29:09 -0700

    moves .css and .js files to respective folders

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://us-central1-clone-a8546.cloudfunctions.net/api",
-  // "http://localhost:5001/clone-a8546/us-central1/api",
-});
-
-export default instance;
